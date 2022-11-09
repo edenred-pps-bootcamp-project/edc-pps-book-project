0905e833a77506462a355b9416639c2bb1ff4050
sorinNgit <sorinnsg@gmail.com>
2022-11-09 18:33:30 +0200

    EDC2 Add User Model Class

---
+++ book-catalogue/src/main/java/Model/User.java
@@ -0,0 +1,48 @@
+package Model;
+
+import java.security.InvalidParameterException;
+
+public class User {
+    private static Long userId = 0L;
+
+    private String firstName;
+    private String lastName;
+
+    static{
+        userId++;
+    }
+
+    public User() throws InvalidParameterException {
+        throw new InvalidParameterException("Provide valid parameters to the constructor");
+    }
+
+    public User(String firstName, String lastName) {
+        // increment static id
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
