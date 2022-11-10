bb31ebfd8a3091d4293ffa9529410360c82b9aed
Alex Tocu <alexandru.tocu@edenred.com>
2022-11-10 11:49:25 +0200

    Adding displayAuthorBooks method

--- book-info/src/main/java/com/edc/pps/Main.java
+++ book-info/src/main/java/com/edc/pps/Main.java
@@ -8,11 +8,14 @@ public class Main {
 
 
         bookService.createBook("Ana are mere", "Ana");
+        bookService.createBook("Ana nu are mere", "Ana");
+        bookService.createBook("Ana are si nu are mere", "Ana");
         bookService.createBook("Maria are mere", "Maria");
         bookService.createBook("Andrei are mere", "Andrei");
         bookService.createBook("Cosmin are mere", "Cosmin");
         bookService.createBook("Florin are mere", "Florin");
         bookService.displayAllBooks();
+        bookService.displayAuthorBooks("Ana");
 
     }
 }
