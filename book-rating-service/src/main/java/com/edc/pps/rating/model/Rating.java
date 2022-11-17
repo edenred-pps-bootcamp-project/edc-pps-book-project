f62580be1b325ab757ba017f13dbc4ad401bf595
lsandu <sandu.doru.luigi@gmail.com>
2022-11-17 10:07:53 +0200

    deleted to do's from rating class and formatted

--- book-rating-service/src/main/java/com/edc/pps/rating/model/Rating.java
+++ book-rating-service/src/main/java/com/edc/pps/rating/model/Rating.java
@@ -1,25 +1,10 @@
 package com.edc.pps.rating.model;
-// TODO: remove unused imports
-// TODO: format this file
 
-// TODO: Comparable is generic, use <>
 public class Rating implements Comparable<Rating> {
-
-    // TODO: the id should have type Long
-    // using the Long object, in case the id is missing it will be null
-    // using primitive long, in case the id is missing it will be 0
     private long ratingId;
-
-    // TODO: no need to initialize
-    // in intellij gray means unused or redundant, you can see that by hovering a gray value
     private long bookId;
     private long userId;
-
-    // TODO: decide between int and Integer
-    // having 0 as default for a book means that all books are trash :)
-    // having null as default means that a book has no rating yet
     private int ratingValue;
-
 
     public Rating(long bookId, long userId, int rating) {
         this.ratingId = bookId * 10000 + userId;
@@ -28,9 +13,6 @@ public class Rating implements Comparable<Rating> {
         this.ratingValue = rating;
         //getAverageRating();
     }
-
-    // TODO: this should be the last method in the class
-
 
     @Override
     public int compareTo(Rating otherRating) {
