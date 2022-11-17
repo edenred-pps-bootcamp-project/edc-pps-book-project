cdc9f0a1323feb56f146bc2b4d300329221eecc7
sorinNgit <sorinnsg@gmail.com>
2022-11-17 13:52:17 +0200

    EDC6 Added Logger and Command Line Runner

--- book-catalog-service/src/main/java/com/edc/pps/catalog/BookCatalogApp.java
+++ book-catalog-service/src/main/java/com/edc/pps/catalog/BookCatalogApp.java
@@ -14,35 +14,6 @@ public class BookCatalogApp {
 
     public static void main(String[] args) {
 
-        Book book1 = new Book("Abecedar", "Cezar Petrescu");
-
-        Book book2 = new Book("Fram Ursul Polar", "Cezar Petrescu");
-
-        System.out.println(book1);
-        Book book3 = new Book("Colt Alb", "Nu stiu");
-
-        System.out.println(book1);
-
-        System.out.println("Welcome to green team's playground! :)");
-
-        // creating a list of books
-        List<Book> userListOfBooks = new ArrayList<>();
-
-
-        // adding books to the list
-        userListOfBooks.add(book1);
-        userListOfBooks.add(book2);
-        userListOfBooks.add(book3);
-        UserService userService = UserService.getInstance();
-
-        userService.createExplicit("Norin", "Seculae","sneculae");
-
-        System.out.println(book1);
-
-        System.out.println("Welcome to green team's playground! :)");
-
-        userService.findAll();
-
         SpringApplication.run(BookCatalogApp.class);
 
     }
