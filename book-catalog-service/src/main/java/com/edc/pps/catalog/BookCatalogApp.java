dedf9877f8de660978a73f0bdcfed718678584cb
sorinNgit <sorinnsg@gmail.com>
2022-11-17 11:18:26 +0200

    EDC6 Added spring dependency in catalog service

--- book-catalog-service/src/main/java/com/edc/pps/catalog/BookCatalogApp.java
+++ book-catalog-service/src/main/java/com/edc/pps/catalog/BookCatalogApp.java
@@ -3,9 +3,13 @@ package com.edc.pps.catalog;
 
 import com.edc.pps.catalog.service.UserService;
 import com.edc.pps.info.model.Book;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.ArrayList;
 import java.util.List;
+
+@SpringBootApplication
 public class BookCatalogApp {
 
     public static void main(String[] args) {
@@ -38,6 +42,8 @@ public class BookCatalogApp {
         System.out.println("Welcome to green team's playground! :)");
 
         userService.findAll();
+
+        SpringApplication.run(BookCatalogApp.class);
 
     }
 
