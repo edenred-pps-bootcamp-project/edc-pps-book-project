399abded438a25a134c07c8447df06bf19cf79c5
Luigi Sandu <sandu.doru.luigi@gmail.com>
2022-11-19 13:31:48 +0200

    Transformed Rating class into an entity to map it to the db.

--- book-rating-service/src/main/java/com/edc/pps/rating/model/Rating.java
+++ book-rating-service/src/main/java/com/edc/pps/rating/model/Rating.java
@@ -1,17 +1,31 @@
 package com.edc.pps.rating.model;
 
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "rating")
 public class Rating implements Comparable<Rating> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long ratingId;
+    @Column(name = "book_id")
     private long bookId;
+    @Column(name = "user_id")
     private long userId;
+    @Column(name = "rating_value")
     private int ratingValue;
+
+    public Rating() {
+    }
 
     public Rating(long bookId, long userId, int rating) {
         this.ratingId = bookId * 10000 + userId;
         this.bookId = bookId;
         this.userId = userId;
         this.ratingValue = rating;
-        //getAverageRating();
     }
 
     @Override
@@ -23,16 +37,32 @@ public class Rating implements Comparable<Rating> {
         return ratingId;
     }
 
+    public void setRatingId(long ratingId) {
+        this.ratingId = ratingId;
+    }
+
     public long getBookId() {
         return bookId;
+    }
+
+    public void setBookId(long bookId) {
+        this.bookId = bookId;
     }
 
     public long getUserId() {
         return userId;
     }
 
-    public int getRating() {
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public int getRatingValue() {
         return ratingValue;
+    }
+
+    public void setRatingValue(int ratingValue) {
+        this.ratingValue = ratingValue;
     }
 
     @Override
