1468d4e42e5d2b4e0f7e2d84669f1418d4cea350
sorinNgit <sorinnsg@gmail.com>
2022-11-21 16:22:30 +0200

    EDC8 Adding User Mapper and User Request/Response

---
+++ book-catalog-service/src/main/java/com/edc/pps/catalog/dto/UserResponse.java
@@ -0,0 +1,52 @@
+package com.edc.pps.catalog.dto;
+
+import java.util.List;
+
+public class UserResponse {
+
+    private  Long userId;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private List<CatalogItem> catalogItems;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public List<CatalogItem> getCatalogItems() {
+        return catalogItems;
+    }
+
+    public void setCatalogItems(List<CatalogItem> catalogItems) {
+        this.catalogItems = catalogItems;
+    }
+}
