e980e7367ccca5abe2ef393322cad2d4763c0ca0
lsandu <sandu.doru.luigi@gmail.com>
2022-11-23 11:15:51 +0200

    EDC11 - Added Unit Test for RatingMapper and Integration Test for RatingService

---
+++ book-rating-service/src/test/java/com/edc/pps/rating/RatingServiceTest.java
@@ -0,0 +1,44 @@
+package com.edc.pps.rating;
+
+import com.edc.pps.rating.dto.RatingMapper;
+import com.edc.pps.rating.repository.RatingRepository;
+import com.edc.pps.rating.service.RatingService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class RatingServiceTest {
+
+    @Mock
+    private RatingRepository ratingRepository;
+
+    @Mock
+    private RatingMapper ratingMapper;
+
+    @InjectMocks
+    private RatingService ratingService;
+
+    @Test
+    void givenRatingRequest_whenSaveOrUpdate_thenReturnRatingResponse(){
+
+    }
+
+    @Test
+    void whenFindAll_thenReturnRatingResponseList(){
+
+    }
+
+    @Test
+    void delete(){
+
+    }
+    @Test
+    void givenBookId_whenGetAllRatingsForBook_thenReturnRatingResponseList(){
+
+
+    }
+
+}
