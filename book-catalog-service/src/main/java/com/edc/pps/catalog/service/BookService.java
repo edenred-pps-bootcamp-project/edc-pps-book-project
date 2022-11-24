b933a86d047d0b10dd872e1f9542d5cab044990d
sorinNgit <sorinnsg@gmail.com>
2022-11-24 14:00:18 +0200

    EDC12 Created find by id method in book service

--- book-catalog-service/src/main/java/com/edc/pps/catalog/service/BookService.java
+++ book-catalog-service/src/main/java/com/edc/pps/catalog/service/BookService.java
@@ -49,6 +49,16 @@ public class BookService {
     }
 
     /**
+     * Get book by id
+     * @param id The id of the book we want to return
+     * @return bookResponse
+     */
+    public BookResponse findById(Long id){
+        log.debug("finding book with id: {}", id);
+        return restTemplate.getForObject(INFO_RESOURCE + "/" + id, BookResponse.class);
+    }
+
+    /**
      * Updates book with the provided id
      * @param id The id of the book we want to update
      */
