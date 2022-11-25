975b7e65029960655b7b1086127df7f8a43df609
daria68 <dariadirstariu493@gmail.com>
2022-11-25 12:02:57 +0200

    EDC11 Updates on RatingService RestTemplate

--- book-catalog-service/src/main/java/com/edc/pps/catalog/service/RatingService.java
+++ book-catalog-service/src/main/java/com/edc/pps/catalog/service/RatingService.java
@@ -1,5 +1,6 @@
 package com.edc.pps.catalog.service;
 
+import com.edc.pps.catalog.dto.info.BookResponse;
 import com.edc.pps.catalog.dto.rating.RatingMapper;
 import com.edc.pps.catalog.dto.rating.RatingRequest;
 import com.edc.pps.catalog.dto.rating.RatingResponse;
@@ -18,63 +19,48 @@ import java.util.Optional;
 @Service
 public class RatingService {
 
-    private static final String RATING_RESOURCE = "http://localhost:8081/api/ratings";
-    private final RatingMapper ratingMapper;
-    private final RatingRepository ratingRepository;
+    private static final String RATING_RESOURCE = "http://localhost:8082/api/ratings";
+
     private final RestTemplate restTemplate;
 
     @Autowired
-    public RatingService(RatingRepository ratingRepository, RatingMapper ratingMapper, RestTemplate restTemplate) {
-        this.ratingRepository = ratingRepository;
-        this.ratingMapper = ratingMapper;
+    public RatingService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
+    /**
+     *
+     * @param request
+     * @return
+     */
     public RatingResponse saveOrUpdate(RatingRequest request) {
         log.info("saved rating to db: {}", request);
-        //get all ratings from db
-        List<Rating> ratings = ratingRepository.findAll();
-        //check if the user already rated the book
-        long result = ratings.stream().filter(entry -> entry.getBookId() == request.getBookId() && entry.getUserId() == request.getUserId()).count();
-
-        //if the user never rated the book it will be added as a new entry in db
-        if (result == 0) {
-            Rating rating = ratingMapper.toEntity(request);
-            ratingRepository.save(rating);
-            return ratingMapper.toDto(rating);
-        }
-        //if the user already rated the book it will be updated
-        else {
-            Rating foundRating = ratingRepository.findByBookIdAndUserId(request.getBookId(), request.getUserId());
-            foundRating.setRatingValue(request.getRatingValue());
-            ratingRepository.save(foundRating);
-            return ratingMapper.toDto(foundRating);
-        }
+        return restTemplate.postForObject(RATING_RESOURCE, request, RatingResponse.class);
     }
 
+    /**
+     *
+     * @return
+     */
     public List<RatingResponse> findAll() {
         log.debug("getting all ratings...");
-        List<Rating> ratings = ratingRepository.findAll();
-        return ratingMapper.toDto(ratings);
+
+        return (List<RatingResponse>) restTemplate.getForObject(RATING_RESOURCE, RatingResponse.class);
     }
 
-    // RESTTemplate delete method
+    /**
+     *
+     * @param id
+     */
     public void delete(Long id) {
-        //1. rating with id was not found
-        Optional<Rating> rating = ratingRepository.findByRatingId(id);
-        if (rating.isEmpty()) {
-            throw new RatingNotFoundException("no rating with id: " + id);
-        } else {
-            log.debug("deleting rating with id: {}", id);
-            restTemplate.delete(RATING_RESOURCE + "/" + id);
-        }
+        log.info("deleting rating {}", id);
+        restTemplate.delete(RATING_RESOURCE + "/" + id);
     }
 
     // method to get all ratings for a certain book to calculate average
 
     public List<RatingResponse> getAllRatingsForBook(long bookId) {
         log.debug("getting all rating for bookId: {}", bookId);
-        List<Rating> ratings = ratingRepository.findByBookId(bookId);
-        return ratingMapper.toDto(ratings);
+        return (List<RatingResponse>) restTemplate.getForObject(RATING_RESOURCE + "/" + bookId, RatingResponse.class);
     }
 }
