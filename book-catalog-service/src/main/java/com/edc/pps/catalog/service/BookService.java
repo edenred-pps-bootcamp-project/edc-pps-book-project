b26257b556b9bb5942fcd5249a02ed12745324f5
sorinNgit <sorinnsg@gmail.com>
2022-12-08 19:30:48 +0200

    EDC15 Updated Port on Book Info Service Rest Template

--- book-catalog-service/src/main/java/com/edc/pps/catalog/service/BookService.java
+++ book-catalog-service/src/main/java/com/edc/pps/catalog/service/BookService.java
@@ -14,7 +14,7 @@ import java.util.Map;
 @Slf4j
 @Service
 public class BookService {
-    private static final String INFO_RESOURCE = "http://localhost:8082/api/books";
+    private static final String INFO_RESOURCE = "http://localhost:8084/api/books";
     private final RestTemplate restTemplate;
 
     @Autowired
